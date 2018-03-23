b6a0c33a52a53141837c3d1707c89fea01f0833c
Mark Shelton <mark@shelton.id.au>
2018-03-23 11:26:15 +0800

    haskell | leap | submit

---
+++ haskell/leap/src/LeapYear.hs
@@ -0,0 +1,12 @@
+module LeapYear (isLeapYear) where
+
+isLeapYear :: Integer -> Bool
+isLeapYear year
+    | year `mod` 400 == 0 = True
+    | year `mod` 100 == 0 = False
+    | year `mod` 4 == 0 = True  
+    | otherwise = False
+
+-- on every year that is evenly divisible by 4
+-- except every year that is evenly divisible by 100
+-- unless the year is also evenly divisible by 400
