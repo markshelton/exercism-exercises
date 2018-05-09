a357ff646d6b2b651f351f8d479bc90acf075929
Mark Shelton <mark@shelton.id.au>
2018-05-09 19:38:15 +0800

    haskell | sublist | iteration 2

--- haskell/sublist/src/Sublist.hs
+++ haskell/sublist/src/Sublist.hs
@@ -1,20 +1,12 @@
 module Sublist (Sublist(..), sublist) where
 
+import Data.List (isInfixOf)
+
 data Sublist = Equal | Sublist | Superlist | Unequal deriving (Eq, Show, Ord, Enum, Bounded)
 
 sublist :: Eq a => [a] -> [a] -> Sublist
 sublist xs ys
-    | xs == ys = Equal
-    | xs `elem` sublists (length xs) (length ys) ys = Sublist
-    | ys `elem` sublists (length ys) (length xs) xs = Superlist
-    | otherwise = Unequal
-
-sublists :: Int -> Int -> [a] -> [[a]]
-sublists min max xs = concatMap (sublistLength xs) [min..max]
-
-sublistLength :: [a] -> Int -> [[a]]
-sublistLength _ 0 = [[]]
-sublistLength [] _ = []
-sublistLength s@(_:xs) n
-    | n <= length s = take n s : sublistLength xs n
-    | otherwise = []
+    | xs == ys          = Equal
+    | xs `isInfixOf` ys = Sublist
+    | ys `isInfixOf` xs = Superlist
+    | otherwise         = Unequal
