257a2e0281f9fe7a89153bfa23e1039eadee3573
Mark Shelton <mark@shelton.id.au>
2018-05-09 14:46:26 +0800

    haskell | pig-latin | submit

---
+++ haskell/pig-latin/src/PigLatin.hs
@@ -0,0 +1,43 @@
+module PigLatin (translate) where
+
+import Data.Tuple (swap)
+import Data.List (sort, group, (\\), span)
+import Control.Monad (filterM)
+
+data Syllable = Vowel | Consonant String deriving (Eq, Show)
+
+alphabet = ['a'..'z']
+vowels = ['a','e','i','o','u']
+vowelsInner = 'y':vowels
+consonants = alphabet \\ vowelsInner
+
+translate :: String -> String
+translate = unwords . map translateWord . words
+
+translateWord :: String -> String
+translateWord xs = case checkStart xs of
+    Nothing -> xs
+    Just Vowel -> xs ++ "ay"
+    Just (Consonant ys) -> ys ++ "ay"
+
+checkStart :: String -> Maybe Syllable
+checkStart [] = Nothing
+checkStart [a] = Nothing
+checkStart s@(a:b:xs)
+    | a `elem` vowels = Just Vowel
+    | [a,b] `elem` ["xr", "yt"] = Just Vowel
+    | otherwise = Just (Consonant s')
+    where 
+        s' = concatTuple . swap $ checkConsonant' s
+        checkConsonant' s = checkConsonant s ("","")
+        concatTuple (x,y) = concat (x : [y])
+
+
+checkConsonant :: String -> (String, String) -> (String, String)
+checkConsonant [] parts = parts
+checkConsonant [a] parts = parts
+checkConsonant (a:b:xs) (start, rest)
+    | [a,b] == "qu" = checkConsonant xs (start ++ [a,b], rest)
+    | a `elem` vowels = (start, a:b:xs)
+    | b `elem` vowelsInner = (start ++ [a], b:xs)
+    | otherwise = checkConsonant (b:xs) (start ++ [a], rest)
